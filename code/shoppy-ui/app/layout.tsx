a02510153cc099d94c66c26bf0e3f0051a2796e0
AlfMeodel <alf.meodel@gmail.com>
2024-11-16 21:44:02 +0100

    Backup before Next.js version change

--- code/shoppy-ui/app/layout.tsx
+++ code/shoppy-ui/app/layout.tsx
@@ -1,6 +1,14 @@
+"use client"
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@emotion/react";
+import darkTheme from "./dark.theme";
+import { CssBaseline } from "@mui/material";
+import { Container } from "@mui/material";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +36,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Container>
+              {children}
+            </Container>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
